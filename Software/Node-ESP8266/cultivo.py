a17a59d61b3dcceac05dbb0d18e313ff4902ddf8
fandres <fabian.salamanca@openmailbox.org>
2017-09-25 21:37:33 -0500

    imports PEP-8 printDebugMode

--- Software/Node-ESP8266/cultivo.py
+++ Software/Node-ESP8266/cultivo.py
@@ -3,29 +3,30 @@ from config import topico
 from DHT22 import DHT22
 import time
 
-class cultivo():
 
-    def __init__(self,debug):
-        super(cultivo, self).__init__()
-        self.debug=debug
-        self.sensores={}
-        self.data={}
-        #self.sensor=sens
+class Cultivo():
+
+    def __init__(self, debug):
+        super(Cultivo, self).__init__()
+        self.debug = debug
+        self.sensores = {}
+        self.data = {}
+        # self.sensor=sens
         self.crear_cultivo()
 
-    def crear_cultivo(self,sensor=sens):
-        for i,n in sens.items():
-            self.add_sensor(i,n)
+    def crear_cultivo(self, sensor=sens):
+        for i, n in sens.items():
+            self.add_sensor(i, n)
 
-    def add_sensor(self, name,tipo):
-        if tipo=="DHT22":
-            self.sensores[name]=DHT22(self.debug,name,pin_dht=4)
+    def add_sensor(self, name, tipo):
+        if tipo == "DHT22":
+            self.sensores[name] = DHT22(self.debug, name, pin_dht=4)
 
     def read_sensores(self):
         for i in sens:
-            self.data[i]=self.sensores[i].readData()
+            self.data[i] = self.sensores[i].readData()
             time.sleep_ms(500)
-        self.debug.print(self.data)
+        self.debug.printDebug(self.data)
 
     def send_data(self):
         pass
