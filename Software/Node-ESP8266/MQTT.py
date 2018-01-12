112610eeeea3a73d1d675a08e52641a633ce3936
fandres <fabian.salamanca@openmailbox.org>
2018-01-12 17:50:58 -0500

    MQTT: user/password authentication

--- Software/Node-ESP8266/MQTT.py
+++ Software/Node-ESP8266/MQTT.py
@@ -1,7 +1,7 @@
 from umqtt.simple import MQTTClient
 from ubinascii import hexlify
 from machine import unique_id
-from config import BROKER
+from config import BROKER, PORT, NAME, MQTT_PASS
 from config import topico
 
 
@@ -13,7 +13,7 @@ class MQTT():
         super(MQTT, self).__init__()
         self.debug = debug
         self.CLIENT_ID = hexlify(unique_id())
-        self.client_mqtt = MQTTClient(self.CLIENT_ID, BROKER)
+        self.client_mqtt = MQTTClient(self.CLIENT_ID, BROKER, PORT, NAME, MQTT_PASS)
         self.debug_mode = 1  # Debug On:1 | Off:0
 
     def connect(self):
