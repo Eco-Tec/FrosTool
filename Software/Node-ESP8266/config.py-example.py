83f62dbe43ccd87a682a88c38871a0b4060f7c1d
fandres <fabian.salamanca@openmailbox.org>
2018-01-17 17:06:02 -0500

    Update config.example.py

--- Software/Node-ESP8266/config.py-example.py
+++ Software/Node-ESP8266/config.py-example.py
@@ -1,9 +1,10 @@
 ﻿CONFIG = b""
+SSID = "my_red"
 PASSWORD = "my_password"
 BROKER = "192.168.0.16"
-SSID = "my_red"
+PORT = 8883
 NAME = "CLIENT_ID"
-MQTT_PASS = b"my_mqtt_password"
-topico = "/cultivo/"
+#MQTT_PASS = b"my_mqtt_password"
 topic_key = ""
 sensor_list = {"sensor 1": "DHT22", "sensor 2": "DHT22", "sensor 3": "DHT22"}
+key = "firmware_1.4"
